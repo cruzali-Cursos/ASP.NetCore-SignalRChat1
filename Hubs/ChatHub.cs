b7329b42ef5f1b73976cefbfb8d4c6d50d8dbe72
Ali <ali.cruz@me.com>
2023-05-19 07:04:51 -0600

    Carga inicial https://www.youtube.com/watch?v=lB15IMgAwxw&t=9s&ab_channel=gavilanch2

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // Esta funcion se ejecutará desde JS.
+
+        // Distribuye el mensaje a todos los clientes conectados
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+
+    }
+}
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,21 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", (user, message) => {
+    const msg = message.replace(/&/g, "&amp;").replace(/</g, "$lt;").replace(/>/g, "@gt");
+    const fecha = new Date().toLocaleTimeString();
+    const mensajeAMostrar = fecha + "<strong>" + user + "</strong>:" + msg;
+    const li = document.createElement("li");
+    li.innerHTML = mensajeAMostrar;
+
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().catch(err => console.error(err.toString()));
+
+document.getElementById("sendButton").addEventListener("click", event => {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(err => console.error(error.toString));
+    event.preventDefault();
+});
