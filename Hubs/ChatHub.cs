63290d086e9f175c27f21f5d6a7f3df2c5d766d2
Ali <ali.cruz@me.com>
2023-07-09 14:35:28 -0600

    push

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRChat1.Models;
 using System.Threading.Tasks;
 
 namespace SignalRChat1.Hubs
 {
     public class ChatHub : Hub
     {
-        // Esta funcion se ejecutará desde JS.
+        private readonly string _botUser;
 
+        public ChatHub()
+        {
+            _botUser = "MyChat Bot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room)
+                        .SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} se ha unido {userConnection.Room}");
+        }
+
+        // Esta funcion se ejecutará desde JS.
         // Distribuye el mensaje a todos los clientes conectados
 
         public async Task SendMessage(string user, string message)
---
+++ Models/UserConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRChat1.Models
+{
+    public class UserConnection
+    {
+        public string User { get; set; }
+        public string Room { get; set; }
+    }
+}
